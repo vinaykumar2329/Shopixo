bea06f9a5645b82d810192672c77a8f0c2617fb2
vinay <vinaykumar2329@gmail.com>
2026-01-29 16:11:39 +0530

    ressponsiveness added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 const Hero = ({hero}) => {
   return (
-    <div className=' flex flex-col h-[90vh] bg-gray-700 justify-center text-center w-full px-4 gap-3'
-    style={{backgroundImage:`url(${hero})`,backgroundSize:"cover",backgroundPosition:"center"}}>
+    <div className='hidden md:flex flex-col h-[90vh] bg-gray-700  w-full px-4 gap-3'
+    style={{backgroundImage:`url(${hero})`,backgroundSize:"cover",backgroundPosition:"center" }}>
     </div>
     
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import logo from '../assets/Shopixo.svg'
+import logo1 from '../assets/S.svg'
 import { Link } from 'react-router-dom'
+import { CgProfile } from "react-icons/cg";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaShoppingCart } from "react-icons/fa";
 const Navbar = () => {
   return (
     <>
-    <nav className='flex justify-between p-4'>
+    <nav className='flex justify-between items-center  w-full  p-4'>
       <div className="leftNav">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className='hidden md:block' />
+        <img src={logo1} alt="logo"className='md:hidden h-5 w-5'/>
+      
       </div>
 
-      <div className="rightNav">
-        <ul className='flex gap-3'>
-          <input type="search" placeholder='Search' className='bg-gray-700 px-1 rounded outline-none ' />
-
+      <div className="centerNav">
+        <ul className='md:flex gap-6 hidden'>
           <Link to='/'>Home</Link>
           <Link to='/men'>Men</Link>
           <Link to='/women'>Women</Link>
           <Link to='/kids'>Kids</Link>
         </ul>
       </div>
-
+      <div className="rightNav flex justify-end gap-3 text-center">
+        <input type="search" placeholder='Search' className='bg-gray-700 px-1 rounded outline-none ' />
+        <FaShoppingCart className='text-xl' />
+        <CgProfile className='text-xl' />
+        <GiHamburgerMenu className='text-xl md:hidden'  />
+      </div>
     </nav>
     </>
   )
