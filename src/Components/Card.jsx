2dc718233acb033ca8e8bdf65a6442cf15ddffeb
vinay <vinaykumar2329@gmail.com>
2026-01-30 14:34:21 +0530

    product listed using api

---
+++ src/Components/Card.jsx
@@ -0,0 +1,21 @@
+ const Card = ({ item }) => {
+  return (
+    <div className="w-[180px] flex-shrink-0 cursor-pointer bg-white text-black p-2 border-2">
+      <img
+        src={item.image}
+        alt=""
+        className="h-[240px] w-full object-contain"
+      />
+
+      <p className="text-sm font-semibold mt-2 truncate">
+        {item.title}
+      </p>
+
+      <p className="text-gray-500 text-xs">
+        Min. {Math.floor(Math.random() * 40 + 50)}% Off
+      </p>
+    </div>
+  );
+};
+
+export default Card;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from '../Components/Hero'
 import  hero from '../assets/hero.png'
+import axios from 'axios'
+import Card from '../Components/Card'
 
 const Home = () => {
+const [data, setdata] = useState([])
+  useEffect(() => {
+    const fetchProduct =async()=>{
+    try{
+      const res =await axios.get("https://fakestoreapi.com/products")
+
+    console.log(res.data)
+    setdata(res.data)
+    
+    }
+    catch (err){
+      console.error(err)
+    }
+  }
+  fetchProduct()
+  }, [])
+  
   return (
     <>
     <Hero hero={hero}/>
+    <div className='flex md:gap-10 overflow-x-auto hide-scrollbar p-3 md:flex-wrap md:justify-center'>  
+      {data.map((item)=>{
+        return <Card key={item.id} item={item}/>
+      })}
+    </div>
+
     </>
   )
 }
